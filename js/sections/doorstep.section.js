8899d2f96e100d9695250cbe6039517bccf6656e
niloykarccusa <niloy.kar@codeclouds.co.in>
2026-01-19 20:12:21 +0530

    Update doorstep.section.js

--- js/sections/doorstep.section.js
+++ js/sections/doorstep.section.js
@@ -5,35 +5,13 @@ export function createDoorstepSection(el) {
 
     const clip = {
         hiddenLeft: "polygon(0 0, 0 0, 0 100%, 0 100%)",
-        visible: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-        hiddenRight: "polygon(100% 0, 100% 0, 100% 100%, 100% 100%)",
-        invisible:"polygon(0 0,0 0,0 0,0 0)"
+        visible: "polygon(0 0, 100% 0, 100% 100%, 0 100%)"
     };
 
     let tl;
 
-    function kill() {
-        if (tl) {
-            tl.kill();
-            tl = null;
-        }
-    }
-
-    function reset() {
-        gsap.set([heading, text], {
-            y: -120,
-            opacity: 0
-        });
-
-        gsap.set(grid, {
-            clipPath: clip.hiddenLeft
-        });
-    }
-
-    reset();
-
-    function animateIn() {
-        tl = gsap.timeline();
+    function buildTimeline() {
+        tl = gsap.timeline({ paused: true });
 
         tl.fromTo(
             [heading, text],
@@ -55,46 +33,18 @@ export function createDoorstepSection(el) {
             },
             "<0.1"
         );
-        // return tl;
-    }
-
-    function animateOut() {
-        tl = gsap.timeline();
-
-        tl.fromTo(
-            grid,
-            { clipPath: clip.visible },
-            {
-                clipPath: clip.hiddenRight,
-                duration: 1.6,
-                ease: "power3.out"
-            },
-        )
-        .to(
-            [heading, text],
-            {
-                y: -120,
-                opacity: 0,
-                duration: 1.4,
-                ease: "power3.out"
-            },
-            "<"
-        )
-        .add(reset);
-        // return tl;
     }
 
     return {
         el,
 
-        onEnter(direction) {
-            kill();
-            animateIn();
+        onEnter() {
+            if (!tl) buildTimeline();
+            tl.play();
         },
 
-        onLeave(direction) {
-            kill();
-            animateOut();
+        onLeave() {
+            if (tl) tl.reverse();
         }
     };
 }
