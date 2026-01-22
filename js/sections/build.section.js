16b1d294d6c244e6d1138b7e18e2dd0b5344b24e
niloykarccusa <niloy.kar@codeclouds.co.in>
2026-01-22 13:19:55 +0530

    Update build.section.js

--- js/sections/build.section.js
+++ js/sections/build.section.js
@@ -1,37 +1,53 @@
 export function createBuildSection(el) {
-    const blogGrid = el.querySelector(".blog-grid");
-    let tl;
+  const header = el.querySelector(".build-header");
+  const blogGrid = el.querySelector(".blog-grid");
+  let tl;
 
-    function buildTimeline() {
-        tl = gsap.timeline({
-            paused: true,
-            defaults: { duration: 1, ease: "power3.out" }
-        });
+  function buildTimeline() {
+    tl = gsap.timeline({
+      paused: true,
+      defaults: { ease: "power3.out" }
+    });
 
-        tl.fromTo(
-            blogGrid,
-            {
-                autoAlpha: 0,
-                xPercent: -100
-            },
-            {
-                autoAlpha: 1,
-                xPercent: 0
-            },
-            1.5
-        );
+    tl.fromTo(
+      blogGrid,
+      {
+        autoAlpha: 0,
+        xPercent: -100
+      },
+      {
+        autoAlpha: 1,
+        xPercent: 0,
+        duration: 2.5
+      },
+      0
+    );
+
+    tl.fromTo(
+      header,
+      {
+        y: -120,
+        autoAlpha: 0
+      },
+      {
+        y: 0,
+        autoAlpha: 1,
+        duration: 0.8
+      },
+      1
+    );
+  }
+
+  return {
+    el,
+
+    onEnter() {
+      if (!tl) buildTimeline();
+      tl.play();
+    },
+
+    onLeave() {
+        tl.reverse();
     }
-
-    return {
-        el,
-
-        onEnter(direction) {
-            if (!tl) buildTimeline();
-            tl.play();
-        },
-
-        onLeave(direction) {
-            
-        }
-    };
+  };
 }
